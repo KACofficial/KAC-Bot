08ecb1903e14df4da31c95ce8715972adba6e529
KACofficial <perryvance00@gmail.com>
2024-08-19 19:42:06 -0500

    Add a !kill_bot command

--- commands/moderation.py
+++ commands/moderation.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
-
+import sys
+from asyncio import TimeoutError
 
 class Moderation(commands.Cog):
     """Commands for neckbeards"""
@@ -28,6 +29,37 @@ class Moderation(commands.Cog):
             await ctx.reply(f"Failed to kick `{member.name}`: {e}")
         except Exception as e:
             await ctx.reply(f"An error occurred: {e}")
+        
+    @commands.command()
+    async def kill_bot(self, ctx: commands.Context):
+        """Kill the bot (owner only)"""
+        if any(role.name == "Owner" for role in ctx.author.roles):
+            conf_message: discord.Message = await ctx.reply("Are you sure?")
+            await conf_message.add_reaction('✅')
+            await conf_message.add_reaction('❌')
+
+            def check(reaction, user):
+                return user == ctx.author and reaction.message.id == conf_message.id
+
+            try:
+                reaction, _ = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
+
+                if reaction.emoji == '✅':
+                    await ctx.send("Shutting down...")
+                    await self.bot.close()
+                elif reaction.emoji == '❌':
+                    await ctx.send("Shutdown cancelled.")
+                else:
+                    await ctx.send("Invalid reaction. Shutdown cancelled.")
+
+            except TimeoutError:
+                await ctx.send("No reaction received. Shutdown cancelled.")
+        else:
+            await ctx.message.add_reaction('❌')
+            await ctx.reply("You do not have permission to shut the bot down!")
+            
+
+    
 
 
 async def setup(bot):
