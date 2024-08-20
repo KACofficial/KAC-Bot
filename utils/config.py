4cb7731e94b6b13d8225663c5e447a399a2581f3
KACofficial <perryvance00@gmail.com>
2024-08-19 19:43:48 -0500

    Add a function for grabbing apod key

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,13 @@
 import json
 
-
 def get_webui_key():
     with open("config.json") as f:
         config = json.load(f)
     return config["webui_key"]
+
+
+def get_apod_key():
+    with open("config.json") as f:
+        config = json.load(f)
+    return config["apod_key"]
+
